91876873cbe0935ec08fd61c01b6863343f6e7be
a-r-t-i-f-i-c-i-a-l <>
2024-10-06 22:56:53 +0200

    Rebuild

---
+++ _app/immutable/chunks/paths.BaPbSWrJ.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_15oc80u)==null?void 0:s.base)??"/interactive-color-theory";var e;const a=((e=globalThis.__sveltekit_15oc80u)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ _app/immutable/entry/start.DECnMQza.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DpwOTI3V.js";export{t as start};
